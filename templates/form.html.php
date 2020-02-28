ac2e5c4ff96ddaf6cca4032d7ed2a0e4461b3884
Sergei Vasilev <vasiliev05@gmail.com>
2020-02-28 08:50:18 +0300

    Add finished form.

--- templates/form.html.php
+++ templates/form.html.php
@@ -3,8 +3,9 @@
     <div class="mdl-grid">
         <div class="mdl-cell mdl-cell--12-col mdl-typography--text-center">
             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <input class="mdl-textfield__input" type="text" id="inn-input">
-                <label class="mdl-textfield__label" for="inn-input">Введите ИНН</label>
+                <input id="inn-input" class="mdl-textfield__input" type="text">
+                <label id="inn-label" class="mdl-textfield__label" for="inn-input">Введите ИНН</label>
+                <span id="inn-message" class="mdl-textfield__error" style="visibility: visible"></span>
             </div>
         </div>
     </div>
@@ -17,48 +18,106 @@
                 Проверить достоверность
             </button>
 
-            <div id="loading-spinner" class="mdl-spinner mdl-spinner--single-color mdl-js-spinner is-active"></div>
+            <div id="loading-spinner" style="display: none" class="mdl-spinner mdl-spinner--single-color mdl-js-spinner is-active"></div>
         </div>
     </div>
 
 </form>
 
 <script>
+    let isLoading = false
     let input = document.getElementById('inn-input')
+    let message = document.getElementById('inn-message')
     let spinner = document.getElementById('loading-spinner')
     let submitButton = document.getElementById('submit-button')
 
     submitButton.addEventListener('click', () => {
-        disableSubmitButton()
-        enableSpinner()
+        if (!checkInnInput()) {
+            return;
+        }
+
+        isLoading = true
+        setTimeout(() => {
+            if (isLoading) {
+                disableSubmitButton()
+                enableSpinner()
+            }
+        }, 100)
 
         checkAuthenticity()
-            .then(() => {
+            .then((e) => {
+                if (e.authenticity !== undefined && e.authenticity) {
+                    console.log(e)
+                    showSuccessMessage(e.message)
+                } else {
+                    console.log(e)
+
+                    showErrorMessage(e.message)
+                }
             })
-            .catch(() => {
+            .catch((e) => {
+                showErrorMessage(e.message)
             })
             .finally(() => {
+                isLoading = false
                 enableSubmitButton()
                 disableSpinner()
             })
     }, false)
 
+    function checkInnInput() {
+        let inn = input.value
+
+        if (inn.length < 10) {
+            showErrorMessage('ИНН слишком короткий')
+            return false;
+        }
+
+        if (inn.length > 12) {
+            showErrorMessage('ИНН слишком длинный')
+            return false;
+        }
+
+        if (!/^\d+$/.test(inn)) {
+            showErrorMessage('ИНН должен содержать только цифры')
+            return false;
+        }
+
+        showEmptyMessage()
+        return true;
+    }
+
     async function checkAuthenticity() {
         let checkPath = submitButton.getAttribute('data-check-path')
+        let inn = input.value
 
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', checkPath);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.onload = () => {
-            if (xhr.status === 200 && xhr.responseText !== newName) {
-                alert('Something went wrong.  Name is now ' + xhr.responseText);
-            }
-            else if (xhr.status !== 200) {
-                alert('Request failed.  Returned status of ' + xhr.status);
-            }
-        };
-        xhr.send(encodeURI('name=' + newName));
+        return await new Promise(resolve => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', checkPath);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onload = () => {
+                resolve(JSON.parse(xhr.response));
+            };
+            xhr.onerror = function () {
+                resolve(undefined);
+                console.error("An error occurred during the XMLHttpRequest");
+            };
+            xhr.send(JSON.stringify({inn: inn}));
+        })
+    }
 
+    function showSuccessMessage(msg = 'Неизвестная ошибка') {
+        message.textContent = msg
+        message.style.color = 'green'
+    }
+
+    function showErrorMessage(msg = 'Неизвестная ошибка') {
+        message.textContent = msg
+        message.style.color = 'red'
+    }
+
+    function showEmptyMessage() {
+        message.textContent = ''
     }
 
     function disableSubmitButton() {
@@ -77,9 +136,3 @@
         spinner.style.display = 'inline-block';
     }
 </script>
-
-<style>
-    #loading-spinner {
-        display: none;
-    }
-</style>
